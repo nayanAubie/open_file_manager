18a0da54c3ee31328303c4ad58fec598c27b6f67
Nayan <nayanbabariya566@gmail.com>
2025-02-12 13:32:35 +0530

    Add doc comments in source files

--- lib/open_file_manager.dart
+++ lib/open_file_manager.dart
@@ -5,8 +5,12 @@ import 'src/open_file_manager_platform_interface.dart';
 
 export 'src/config.dart';
 
+/// Opens the default file manager app.
 Future<bool> openFileManager({
+  /// Configuration for Android platform
   AndroidConfig? androidConfig,
+
+  /// Configuration for iOS platform
   IosConfig? iosConfig,
 }) {
   return OpenFileManagerPlatform.instance.openFileManager(
--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,13 +1,24 @@
+/// Configuration class for Android platform.
 final class AndroidConfig {
+  /// Type of folder to open.
   final FolderType folderType;
 
   AndroidConfig({required this.folderType});
 }
 
+/// Configuration class for iOS platform.
 final class IosConfig {
+  /// Sub folder path to open.
   final String subFolderPath;
 
   IosConfig({required this.subFolderPath});
 }
 
-enum FolderType { recent, download }
+/// Available folder types for Android.
+enum FolderType {
+  /// Recent folder
+  recent,
+
+  /// Download folder
+  download
+}
